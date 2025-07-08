cad466d066203255c516c0217a4f58e1936721f2
mbzron <rojnorman@gmail.com>
2025-07-08 18:36:50 -0500

    install R and linux (Ubuntu) requirements

---
+++ install_linux_requirements.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev \
+ libxml2-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev \
+ libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev cmake
---
+++ install_requirements.r
@@ -0,0 +1,18 @@
+# install requirements for single cell analysis (split over multiple lines for readability)
+install.packages(c(
+  "Seurat",
+  "dplyr",
+  "ggplot2",
+  "magrittr",
+  "gtools",
+  "stringr",
+  "Matrix",
+  "tidyverse",
+  "patchwork",
+  "data.table",
+  "RColorBrewer",
+  "ggpubr",
+  "ggsci",
+  "clustree",
+  "randomcoloR",
+), dependencies = TRUE)
